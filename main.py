38816160daa3849d6577781cbceb841f078102b1
VivasAngelArg <av42969@gmail.com>
2024-07-25 00:24:51 -0300

    24/07/ "Carga consultas"

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+﻿# -*- coding: utf-8 -*-
 
 # Librerías
 import sys
@@ -27,11 +27,11 @@ from tools.view.view import *
 #  Menu principal.
 def menu():
     cleaning() 
-    print ("\nSistema de Facturacio del Supermercado.\n\nMenu Principal.")
-    print ("\n\t1. Menu de Clientes.")
-    print ("\n\t2. Menu de Productos")
-    print ("\n\t3. Menu de Provedores.")
-    print ("\n\t4. Salir.")
+    print("\nSistema de Facturacio del Supermercado.\n\nMenu Principal.")
+    print("\n\t1. Menu de Clientes.")
+    print("\n\t2. Menu de Productos")
+    print("\n\t3. Menu de Provedores.")
+    print("\n\t4. Salir.")
     opcion = raw_input("\n\n\t\tIngrese una opción: ")
     opcion = validate_integer(opcion)
     opcion = validate_range(opcion, 1, 4)
@@ -39,12 +39,12 @@ def menu():
 # Menu Cliente
 def menu_clientes():
     cleaning()   
-    print ("\nSistema de Facturacion del Supermercado.\n\nMenu de Clientes.")
-    print ("\n\t1. Ingresar un nuevo clientes.")
-    print ("\n\t2. Consultar informacion de un cliente")
-    print ("\n\t3. Modificar datos de un cliente.")
-    print ("\n\t4. Eliminar.")
-    print ("\n\t5. <--- Volver al menu principal.")
+    print("\nSistema de Facturacion del Supermercado.\n\nMenu de Clientes.")
+    print("\n\t1. Ingresar un nuevo clientes.")
+    print("\n\t2. Consultar informacion de un cliente")
+    print("\n\t3. Modificar datos de un cliente.")
+    print("\n\t4. Eliminar.")
+    print("\n\t5. <--- Volver al menu principal.")
     opcion = raw_input("\n\n\t\tIngrese una opción: ")
     opcion = validate_integer(opcion)
     opcion = validate_range(opcion, 1, 5)
@@ -58,28 +58,28 @@ while opcion <= range(1, 4) and opcion != 4:
         seleccion = menu_clientes()  # Mostrar el menú de clientes y obtener la opción seleccionada
           # Según la opción seleccionada, realizar la acción correspondiente
         if seleccion == 1: # Opción 1: Ingresar un nuevo cliente
-            print ("\n\tLa interfaz 'Ingresar un nuevo clientes' esta en desarrollo.\n")
+            print("\n\tLa interfaz 'Ingresar un nuevo clientes' esta en desarrollo.\n")
             seleccion = menu_clientes() # Vuelve al menú cliente, hasta que se desarrolle esta funcionalidad
         elif seleccion == 2:  # Opción 2: Consultar información de un cliente
-            print ("\n\tLa interfaz 'Consultar informacion de un cliente' esta en desarrollo.\n")
-            print ("\n")
+            print("\n\tLa interfaz 'Consultar informacion de un cliente' esta en desarrollo.\n")
+            print("\n")
             seleccion = menu_clientes()  # Vuelve al menú cliente, hasta que se desarrolle esta funcionalidad 
         elif seleccion == 3:  # Opción 3: Modificar datos de un cliente
-            print ("\n\tLa interfaz 'Modificar datos de un cliente.' esta en desarrollo.\n")
-            print ("\n")
+            print("\n\tLa interfaz 'Modificar datos de un cliente.' esta en desarrollo.\n")
+            print("\n")
             seleccion = menu_clientes()  # Vuelve al menú cliente, hasta que se desarrolle esta funcionalidad
         elif seleccion == 4:  # Opción 4: Eliminar un cliente
-            print ("\n\tLa interfaz 'Eliminar.' esta en desarrollo.\n")
-            print ("\n")
+            print("\n\tLa interfaz 'Eliminar.' esta en desarrollo.\n")
+            print("\n")
             seleccion = menu_clientes() # Vuelve al menú cliente, hasta que se desarrolle esta funcionalidad
         if seleccion == 5: # Opción 5: Volver al menú principal 
-            print ("\n")
+            print("\n")
             opcion = menu() # Vuelve al menú principal
-            print ("\n")
+            print("\n")
     elif opcion == 2: # Si la opción es 2 (Menú de Productos)
-        print ("\n\tMenu de productos esta en desarrollo")
-        opcion = menu()  # Vuelve al menú principal
+            print("\n\tMenu de productos esta en desarrollo")
+            opcion = menu()  # Vuelve al menú principal
     elif opcion == 3: # Si la opción es 2 (Menú de Productos) 
-        print ("\n\tMenu de provedores esta en desarrollo")
-        opcion = menu()  # Vuelve al menú principal
-print ("\n\tA salido exitosamente") # Fin del programa
+            print("\n\tMenu de provedores esta en desarrollo")
+            opcion = menu()  # Vuelve al menú principal
+print("\n\tA salido exitosamente") # Fin del programa
--- python-modules/tools/clientes/clientes.py
+++ python-modules/tools/clientes/clientes.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-import pickle
+
+
 import sys
 sys.path.append("python-modules")
 
@@ -14,34 +15,67 @@ def ingreso_cliente():
     # Ingreso de datos
      listem = []
      print("\nSistema de Facturacio del Supermercado.\n\nMenu Agregar nuevo cliente.")
-     dato = raw_input("\n\tNombre Completo: ")
+     dato = raw_input("\n\tNombre Completo: ") # dato N° 0
      listem.append(dato)
-     dato = raw_input("\n\tNumero de D.N.I: ")
+     dato = raw_input("\n\tNumero de D.N.I: ") # dato N° 1
      dato = int(dato)
      listem.append(dato)
-     dato = raw_input("\n\tDireccion: ")
+     dato = raw_input("\n\tDireccion: ") # dato N° 2
      listem.append(dato)
-     dato = raw_input("\n\tLocalidad: ")
+     dato = raw_input("\n\tLocalidad: ") # dato N° 3
      listem.append(dato)
-     dato = raw_input("\n\tCodigo Postal: ")
+     dato = raw_input("\n\tCodigo Postal: ") # dato N° 4
      dato = int(dato)
      listem.append(dato)
-     dato = raw_input("\n\tNumero de Telefono: ")
+     dato = raw_input("\n\tNumero de Telefono: ") # dato N° 5
      dato = int(dato)
      listem.append(dato)
-     dato = raw_input("\n\tCorreo Electronico: ")
+     dato = raw_input("\n\tCorreo Electronico: ") # dato N° 6
      listem.append(dato)
-     dato = True
-     listem.append(dato)
+     dato = True # dato N° 7 
+     listem.append(dato) 
      
-    # Generar un ID único para el cliente
-     if clientes:
-          id_cliente = (clientes.keys(listem[1]))
-     else:
-          id_cliente = listem[1]
-    
+# Usar el DNI como ID único para el cliente
+     id_cliente = listem[1]
+
+    # Agregar el nuevo cliente al diccionario
      clientes[id_cliente] = listem
     
+    # Mostrar el diccionario de clientes
+     print(clientes)
+    
+     # Guardar los cambios en el archivo
      saving_changes_to_the_file("clientes.dat", clientes)
+     print("\nCliente agregado con éxito!")
 
-ingreso_cliente()
+
+def consulta_cliente():
+     dni_consulta = raw_input("Ingrese DNI del cliente a consultar:")
+     dni_consulta = int(dni_consulta)
+     loop = False
+     
+     for id,datos in clientes.iteritems():
+          if dni_consulta in id:
+               datos = clientes[dni_consulta]
+               print("Cliente existente, para el siguiente D.N.I:", id)
+               print("\n")
+               print("Su Nombre completo es: ", datos[0])
+               print("Su Direccion es: ", datos[2])
+               print("Su Localidad es: ", datos[3])
+               print("Su Codigo Postal es: ", datos[4])
+               print("Su Numero de telefo es: ", datos[5])
+               print("Su Correo Electronico es: ", datos[6])
+               if datos[7] == True:
+                    print("Su estado es ACTIVO")
+               else:
+                    print("Su estado es INACTIVO")
+               loop = True
+          else:
+               print("Usuario no encontrado, Ingrese 1 para registralo como cliente nuevo o 2 si quiere realizar otra consulta: ")
+               opcion = raw_input(": ")
+               if opcion == 1:
+                    ingreso_cliente()
+               else:
+                    consulta_cliente()
+print(clientes)
+consulta_cliente()
